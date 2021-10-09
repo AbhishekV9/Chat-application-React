f68548d7e1f8d9e013d792c51df174adb317e55d
Abhishek <avishekv444@gmail.com>
2021-10-10 03:11:31 +0530

    created dummy data file

---
+++ src/utils/contacts.js
@@ -0,0 +1,193 @@
+const contacts= [
+    {
+        userName:"Ashish",
+        id:1,
+        status:online,
+        chats:[
+            {
+                message:"hi Ashish",
+                sender:'self',
+                timestamps:'12.00 PM'
+            },
+            {
+                message:"hi Abhsihek",
+                sender:'user',
+                timestamps:'12.02 PM'
+            },
+            {
+                message:"how are you??",
+                sender:'self',
+                timestamps:'12.03 PM'
+            },
+            {
+                message:"m fine wat abt u ??",
+                sender:'user',
+                timestamps:'12.05 PM'
+            },
+            {
+                message:"m good, are you free today evening",
+                sender:'self',
+                timestamps:'12.10 PM'
+            },
+            {
+                message:"anytime for u buddy",
+                sender:'user',
+                timestamps:'12.13 PM'
+            },
+            {
+                message:"Ok let's meet today 6.00 PM",
+                sender:'self',
+                timestamps:'12.17 PM'
+            },
+            {
+                message:"yeah that's great :)",
+                sender:'user',
+                timestamps:'12.18 PM'
+            }
+
+        ]
+
+    },
+    {
+        userName:"Preeti",
+        id:2,
+        status:online,
+        chats:[
+            {
+                message:"hi Preeti",
+                sender:'self',
+                timestamps:'01.00 PM'
+            },
+            {
+                message:"hi Abhsihek",
+                sender:'user',
+                timestamps:'01.02 PM'
+            },
+            {
+                message:"how are you??",
+                sender:'self',
+                timestamps:'01.03 PM'
+            },
+            {
+                message:"m fine wat abt u ??",
+                sender:'user',
+                timestamps:'01.05 PM'
+            }
+        ]
+
+    },
+    {
+        userName:"Ayush",
+        id:3,
+        status:online,
+        chats:[]
+    },
+    {
+        userName:"Akansha",
+        id:4,
+        status:online,
+        chats:[
+            {
+                message:"hi Abhsihek",
+                sender:'user',
+                timestamps:'05.02 PM'
+            },
+            {
+                message:"hi Akansha",
+                sender:'self',
+                timestamps:'05.02 PM'
+            },
+            {
+                message:"r u in bilaspur ??",
+                sender:'user',
+                timestamps:'05.03 PM'
+            },
+            {
+                message:"yeah and you ??",
+                sender:'self',
+                timestamps:'05.05 PM'
+            },
+            {
+                message:"m in bhilai ??",
+                sender:'user',
+                timestamps:'05.07 PM'
+            },
+            {
+                message:"ok i will be there next week for college work let's meet that day !! ",
+                sender:'self',
+                timestamps:'05.10 PM'
+            },
+            {
+                message:"done !!!",
+                sender:'user',
+                timestamps:'05.11 PM'
+            }
+        ]
+
+    },
+    {
+        userName:"Avinash",
+        id:5,
+        status:online,
+        chats:[]
+    },
+    {
+        userName:"Ananya",
+        id:6,
+        status:online,
+        chats:[]
+    },
+    {
+        userName:"Vinod",
+        id:7,
+        status:online,
+        chats:[
+            {
+                message:"i will be unable to Attend today's class Abhishek so don't wait for me",
+                sender:'user',
+                timestamps:"08.00 AM"
+            },
+            {
+                message:"Ok will meet you after coaching",
+                sender:'self',
+                timestamps:"08.05 AM"
+            }
+        ]
+    },
+    {
+        userName:"Kirti",
+        id:8,
+        status:online,
+        chats:[]
+    },
+    {
+        userName:"Ritesh",
+        id:9,
+        status:online,
+        chats:[
+            {
+                message:"hi Ritesh",
+                sender:'self',
+                timestamps:'01.00 PM'
+            },
+            {
+                message:"hi Abhsihek",
+                sender:'user',
+                timestamps:'01.02 PM'
+            },
+            {
+                message:"how are you??",
+                sender:'self',
+                timestamps:'01.03 PM'
+            },
+            {
+                message:"m fine wat abt u ??",
+                sender:'user',
+                timestamps:'01.05 PM'
+            }
+        ]
+
+    }
+]
+
+export default contacts;
