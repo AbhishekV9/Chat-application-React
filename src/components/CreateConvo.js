e6dffb2ee67a9ec8bb729c2bf6f78699e167e7f7
Abhishek <avishekv444@gmail.com>
2021-10-12 00:54:12 +0530

    showing all contacts inside contact box

--- src/components/CreateConvo.js
+++ src/components/CreateConvo.js
@@ -1,4 +1,5 @@
-
+import UserCard from './UserCard';
+import contacts from '../utils/contacts';
 
 function createConvo(props){
 
@@ -13,8 +14,10 @@ function createConvo(props){
                     <span className="showheader">Contacts</span>
                     <span className="closeimg"><img src="https://cdn-icons-png.flaticon.com/512/753/753345.png" alt="close" onClick={handleClose}/></span>
                 </div>
-                <div>
-
+                <div className="allconctacs">
+                        {contacts.map((user)=>{
+                            return <UserCard user={user} key={ user.id} />
+                        })}
                 </div>
             </div>
         </div>
