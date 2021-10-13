8899305f564fe1f6009abb56800ec47a08058208
Abhishek <avishekv444@gmail.com>
2021-10-14 03:35:56 +0530

    showing background image on righView

--- src/components/RightView.js
+++ src/components/RightView.js
@@ -6,15 +6,15 @@ function RightView(props){
     console.log("props",user);
     return(
       <div>
-           {user.id===0 ? <div>
-              <h1>Hiiiiii</h1>
+           {user.id===0 ? <div className="user0">
+              <div>
+                { showConvo && <CreateConvo setConvo={setConvo} /> }
+              </div>
            </div>: <div>
              <h1>Byeeeeeeee</h1>
              
             </div>}
-            <div>
-                { showConvo && <CreateConvo setConvo={setConvo} /> }
-            </div>
+            
       </div>
     )
 }
